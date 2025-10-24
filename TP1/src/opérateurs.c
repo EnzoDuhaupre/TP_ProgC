56dac8387a0520c1b030ecd0dd258c4e4b418bd5
EnzoDuhaupre <134526882+EnzoDuhaupre@users.noreply.github.com>
2025-10-24 11:54:08 +0200

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdbool.h> 
 
 int main() {
     int a = 16;
@@ -8,7 +8,8 @@ int main() {
     int addition = a + b;
     int soustraction = a - b;
     int multiplication = a * b;
-    float division = (float)a / b;
+    float division = (float)a / b; 
+    int modulo = a % b;
 
     bool egalite = (a == b);
     bool superieur = (a > b);
@@ -19,12 +20,14 @@ int main() {
     printf("Soustraction de b de a : %d - %d = %d\n", a, b, soustraction);
     printf("Multiplication de a par b : %d * %d = %d\n", a, b, multiplication);
     printf("Division de a par b : %d / %d = %.2f\n", a, b, division);
-    printf("Modulo de a par b (reste de la division) : %d %% %d = %d\n\n", a, b, modulo); 
+    printf("Modulo de a par b (reste de la division) : %d %% %d = %d\n\n", a, b, modulo);
 
     printf("a est-il égal à b ? %s\n", egalite ? "vrai" : "faux");
     printf("a est-il supérieur à b ? %s\n", superieur ? "vrai" : "faux");
 
     return 0;
 }
+
+
 
 
